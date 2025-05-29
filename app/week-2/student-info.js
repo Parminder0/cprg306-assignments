3882a29e516e523a3e6fa1e3091ff739dd3f7649
Parminder0 <parmindersingh150805@gmail.com>
2025-05-29 08:24:33 -0600

    Update student-info.js
    updated my name

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function StudentInfo() {
   return (
     <div>
-      <p>Parminnder Singh Sidhu</p>
+      <p>Parminder Singh Sidhu</p>
       <p>
         <Link href="https://github.com/Parminder0/cprg306-assignments" target="_blank">
           My GitHub Repository
