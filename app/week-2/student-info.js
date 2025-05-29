8558edcce3950baca5580ab035f9e8a7f7a0759d
Parminder0 <parmindersingh150805@gmail.com>
2025-05-29 08:07:52 -0600

    Add Week 2 Assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Parminnder Singh Sidhu</p>
+      <p>
+        <Link href="https://github.com/Parminder0/cprg306-assignments" target="_blank">
+          My GitHub Repository
+        </Link>
+      </p>
+    </div>
+  );
+}
